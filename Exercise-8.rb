94e72e8edaea0ca09ada138b8036d083c83a21e6
David <david.he07@yahoo.ca>
2020-09-07 13:56:36 -0400

    made sure to change name to Exercise

--- Exercise-8.rb
+++ Exercise-8.rb
@@ -3,3 +3,11 @@ formatter = "%{first} %{second} %{third} %{fourth}"
 puts formatter % {first: 1, second: 2, third: 3, fourth: 4}
 puts formatter % {first: "one", second: "two", third: "three", fourth: "four"}
 puts formatter % {first: "hello", second: "how", third: "are", fourth: "you"}
+puts formatter % {first: formatter, second: formatter, third: formatter, fourth: formatter}
+
+puts formatter % {
+  first: "I had this thing.",
+  second: "That you could type up right.",
+  third: "But it didn't sing.",
+  fourth: "So I said goodnight."
+}
