782169c6419ae133076079efb6772380585bde37
David <david.he07@yahoo.ca>
2020-09-06 21:29:57 -0400

    implemented lines of code for exercise 7

---
+++ Exercise-7.rb
@@ -0,0 +1,13 @@
+print "How old are you? "
+age = gets.chomp
+
+print "How cool are you? "
+coolness = gets.chomp 
+
+print "How tall are you in feet? "
+tall = gets.chomp
+
+print "How much do you weigh in lbs? "
+weight = gets.chomp
+
+puts "I am #{age} years old, I am #{coolness} cool, I am #{tall} ft tall, and I weigh #{weight} lbs."
